630c4ab7ec77d26c087e764b6bf725b22be0f590
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-09-25 09:15:14 +0000

    llongest common prefix

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+main() {
+  // var sum = 0;
+  // dynamic n = stdin.readLineSync();
+  // var numberList = [n, n * 2, n * 3];
+  // for (var x in numberList) {
+  //   sum += int.parse(x);
+  // }
+  final long = Solution();
+  final strs = ["flower", "flw", "floight"];
+  final str = long.longestCommonPrefix(strs);
+
+  print(str);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Solution {
+  String longestCommonPrefix(List<String> strs) {
+    if (strs.isEmpty) return "";
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    String prefixWord = strs[0];
+    for (int i = 1; i < strs.length; i++) {
+      while (!strs[i].startsWith(prefixWord)) {
+        prefixWord = prefixWord.substring(0, prefixWord.length - 1);
+        if (prefixWord.isEmpty) return "";
+      }
+    }
+
+    return prefixWord;
   }
 }
+// 1. Write a Dart program6 to print the following string in a specific format Sample String :
+// "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,
+// Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+var value = '''
+Twinkle, twinkle, little star,
+\tHow I wonder what you are!
+\t\tUp above the world so high,
+\t\tLike a diamond in the sky.
+Twinkle, twinkle, little star,
+\tHow I wonder what you are
+''';
+//   print(value);
+//-----------------------------------
+// 2. Write a Dart program to get the Python version you are using
+  // print(Platform.version);
+//------------------------------------
+// 3. Write a Dart program to display the current date and time. Sample Output : Current
+//date and time : 2014-07-05 14:34:14
+//  DateTime date = DateTime.now();
+//  print('current date and time : $date');
+//--------------------------------------
+// 4. Write a Dart program which accepts the radius of a circle from the user and compute
+// the area
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+// 5. Write a Dart program which accepts the user's first and last name and print them in
+// reverse order with a space between them
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
