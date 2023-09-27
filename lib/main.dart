a8b5d626207e9ff26c5f9abaebd6953a8266de55
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-09-27 21:59:59 +0000

    Merge Two Sorted Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,44 @@
 main() {
-  final valid = SolutionIsValid();
-  valid.isValid('(}()');
+  final valid = Solution();
+  ListNode list1 = ListNode(1, ListNode(2, ListNode(4)));
+  ListNode list2 = ListNode(1, ListNode(2, ListNode(3)));
+  final s = valid.mergeTwoLists(list1, list2);
+
+  print(s!.next!.val);
 }
 
-class SolutionIsValid {
-  bool isValid(String s) {
-    final Map<String, String> bracketMap = {
-      ')': '(',
-      '}': '{',
-      ']': '[',
-    };
+// Definition for singly-linked list.
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
 
-    final List<String> stack = [];
+class Solution {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    List<int> list = [];
 
-    for (var char in s.split('')) {
-      if (bracketMap.containsValue(char)) {
-        stack.add(char);
-      } else if (bracketMap.containsKey(char)) {
-        if (stack.isEmpty || stack.removeLast() != bracketMap[char]) {
-          return false;
-        }
-      }
+    while (list1 != null) {
+      list.add(list1.val);
+      list1 = list1.next;
+    }
+    while (list2 != null) {
+      list.add(list2.val);
+      list2 = list2.next;
     }
 
-    return stack.isEmpty;
+    if (list.isEmpty) return null;
+
+    list.sort();
+
+    ListNode tail = ListNode(list[0]);
+    ListNode prev = tail;
+
+    for (int i = 1; i < list.length; i++) {
+      ListNode node = ListNode(list[i]);
+      prev.next = node;
+      prev = node;
+    }
+    return tail;
   }
 }
