ebe5b7ddb84209d04283cfce154ed2e4528ca26d
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-10-25 22:32:57 +0000

    adjutment code

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,29 @@
 main() {
   final solution = Solution();
-  final removeRep = solution.removeElement([1, 5, 1], 5);
+  final removeRep = solution.removeElement([1, 5, 1, 2], 5);
   print(removeRep);
 }
 
-class Solution {
-  int removeElement(List<int> nums, int val) {
-    for (int i = 0; i < nums.length; i++) {
-      nums.remove(val);
-    }
-    nums.remove(val);
-    return nums.length;
-  }
-}
+// class Solution2 {
+//   int removeElement(List<int> nums, int val) {
+//     for (int i = 0; i < nums.length; i++) {
+//       nums.remove(val);
+//     }
+//     nums.remove(val);
+//     return nums.length;
+//   }
+// }
 
 // another  response
-
-class Solution2 {
+class Solution {
   int removeElement(List<int> nums, int val) {
-    List expectedNums = [];
-
-    for (final number in nums) {
-      if (number == val) {
-        expectedNums.add(number);
+    for (int number = 0; number < nums.length; number++) {
+      if (nums[number] != val) {
+        nums.remove(number);
       }
     }
 
-    expectedNums.sort();
-
-    final k = expectedNums.length;
+    final k = nums.length;
 
     return k;
   }
