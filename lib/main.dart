3a8e71d72ec94d45514687a8ce4f1d0232dbd23e
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-11-06 08:39:01 +0000

    88. Merge Sorted Array

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
 import 'dart:math';
 
 main() {
-  final listNode = Solution();
-  listNode.deleteDuplicates(ListNode(1, ListNode(2, ListNode(2, ListNode(1)))));
-}
-
-//* Definition for singly-linked list.
-class ListNode {
-  int val;
-  ListNode? next;
-  ListNode([this.val = 0, this.next]);
+  final solution = Solution();
+  solution.merge([1, 2, 3, 3], 2, [3, 4, 5, 6, 7], 3);
 }
 
 class Solution {
-  ListNode? deleteDuplicates(ListNode? head) {
-    ListNode? listNode = head;
-
-    while (listNode?.next != null) {
-      if (listNode?.val == listNode?.next!.val) {
-        listNode?.next = listNode.next;
+  void merge(List<int> nums1, int m, List<int> nums2, int n) {
+    final newNums1 = [];
+    final newNums2 = [];
+    for (int num = 0; num < nums1.length; num++) {
+      if (num != m) {
+        return newNums1.add(nums1[num]);
       }
-      listNode = listNode?.next;
     }
-    return listNode;
+
+    for (int num = 0; num < nums2.length; num++) {
+      if (num != n) {
+        return newNums2.add(nums2[num]);
+      }
+    }
+    return newNums1.add(newNums2);
   }
 }
