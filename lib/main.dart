53ffb805a49c5cb6221e01d0653ed171af2c3675
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-11-07 06:48:31 +0000

    final solution about merge two array and sort

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,14 @@
-import 'dart:math';
-
 main() {
   final solution = Solution();
-  solution.merge([1, 2, 3, 3], 2, [3, 4, 5, 6, 7], 3);
+  solution.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
 }
 
 class Solution {
   void merge(List<int> nums1, int m, List<int> nums2, int n) {
-    final newNums1 = [];
-    final newNums2 = [];
-    for (int num = 0; num < nums1.length; num++) {
-      if (num != m) {
-        return newNums1.add(nums1[num]);
-      }
-    }
+    nums1.removeRange(m, nums1.length);
+    nums2.removeRange(n, nums2.length);
 
-    for (int num = 0; num < nums2.length; num++) {
-      if (num != n) {
-        return newNums2.add(nums2[num]);
-      }
-    }
-    return newNums1.add(newNums2);
+    nums1.addAll(nums2);
+    nums1.sort();
   }
 }
