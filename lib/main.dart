22f3fc70b5d6b65980ccb2c312b586d2ea4defc6
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-11-11 01:14:39 +0000

    101. Symmetric Tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 main() {
   final solution = Solution();
-  final response = solution.isSameTree(
-      TreeNode(5, TreeNode(2, TreeNode(3)), TreeNode(7)),
-      TreeNode(5, TreeNode(2, TreeNode(3)), TreeNode(7)));
-
+  final response = solution.isSymmetric(TreeNode(2, TreeNode(3), null));
   print(response);
 }
 
@@ -15,14 +12,7 @@ class TreeNode {
 }
 
 class Solution {
-  bool isSameTree(TreeNode? root1, TreeNode? root2) {
-    if (root1 == null && root2 == null) return true;
-    if (root1 == null || root2 == null) return false;
-
-    final result = root1.val == root2.val &&
-        isSameTree(root1.left, root2.left) &&
-        isSameTree(root1.right, root2.right);
-
-    return result;
+  bool isSymmetric(TreeNode? root) {
+    return true;
   }
 }
