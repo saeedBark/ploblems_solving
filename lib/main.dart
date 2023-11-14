e1926938065ef154c4e8f7bcbbebce3ea24a5876
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-11-14 07:58:55 +0000

    fixed small bug

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class Solution {
     int result = 0;
 
     for (int i = 0; i < columnTitle.length; i++) {
-      int remaind = columnTitle.codeUnits[0] - 64;
+      int remaind = columnTitle.codeUnits[i] - 64;
 
       result = result * 26 + remaind;
     }
