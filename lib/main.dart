5ae4286efdd3b0046063f9ebfd97fa336fcadec1
saeed_bark <saeedbark96@gmail.com>
2024-04-03 00:24:51 +0000

    Refactor the addTwoNumbers method in the Solution class to improve readability and efficiency. This includes restructuring the logic for adding two numbers represented by linked lists.

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,40 @@
 main() {
   Solution solution = Solution();
  
- final result =solution.twoSum([2,5,4,8],6);
+ final result =solution.addTwoNumbers(ListNode(5,ListNode(6,ListNode(4))),ListNode(2,ListNode(3,ListNode(4))));
  print(result);
 }
+
+  class ListNode {
+    int val;
+    ListNode? next;
+  ListNode([this.val = 0, this.next,]);
+ }
+ 
 class Solution {
-  List<int> twoSum(List<int> nums, int target) {
-    for(int i = 0; i < nums.length; i++) {
-      for(int j = i + 1; j < nums.length; j++) {
-        if(nums[i] + nums[j] == target) {
-          return [i, j];
-        }
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+      final newL1 = [];
+      final newL2 = [];
+       ListNode finalListNode = ListNode();
+      while (l1 != null){        
+              newL1.insert(0,l1.val);
+              l1 = l1.next;  
       }
-    }
-    throw "No solution found";
+      while (l2 != null){
+              newL2.insert(0,l2.val);
+              l2 = l2.next; 
+      }
+    
+     final sum = ( BigInt.parse(newL1.join('')) +  BigInt.parse(newL2.join('')) );
+     final reversedSumList = sum.toString().split('').reversed;
+     final nodes = reversedSumList.map((e)=>ListNode(int.parse(e))).toList();
+
+     for(var i =0 ; i<nodes.length-1; i++){
+             nodes[i].next = nodes[i+1];
+     }  
+     finalListNode = nodes[0];
+
+     return finalListNode;
   }
 }
 
