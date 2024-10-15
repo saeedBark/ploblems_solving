9b7ba0e2dcd156e9a9455e1e8044b9ce48135f99
saeedBark <saeedbark96@gmail.com>
2024-10-15 08:30:13 +0000

    - there porblem add two numbers in the list node have two solution -  convert the list node to int and add them and convert the result to list node -   use the carry method -  use the linked list traversal method -  use the linked list recursion method -  use the linked list iteration method -  use the linked list stack method -  use the linked list queue method -  use the linked list deque method -  use the linked list priority queue method

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,77 @@
 main() {
-  SolutionSort solution = SolutionSort();
+  Solution solution = Solution();
 
-  solution.twoSum([2, 5, 4, 8], 6);
+  final result = solution.addTwoNumbers(ListNode(2, ListNode(4, ListNode(3))),
+      ListNode(5, ListNode(6, ListNode(4))));
+
+  print(result);
 }
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+// thes solution have a problem when the list node have more than 10 node
+// the solution is not efficient
+// the solution is not scalable
+// the solution is not maintainable
+// the solution is not readable
+// the solution use more memory than the second solution
+// the solution use more time than the second solution
+// the use to convert the list node to int and add them and convert the result to list node is not efficient
 
 class Solution {
-  List<int> twoSum(List<int> nums, int target) {
-    Map<int, int> myMap = {};
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    final newL1 = [];
+    final newL2 = [];
+    ListNode? finalListNode;
+    while (l1 != null) {
+      newL1.insert(0, l1.val);
+      l1 = l1.next;
+    }
+    while (l2 != null) {
+      newL2.insert(0, l2.val);
+      l2 = l2.next;
+    }
+    final sum = (int.parse(newL1.join('')) + int.parse(newL2.join('')));
 
-    for (int i = 0; i < nums.length - 1; i++) {
-      int value = target - nums[i];
-      if (myMap.containsKey(value)) {
-        return [i, myMap[value]!];
-      }
-      myMap[value] = i;
+    final reversedSumList = sum.toString().split('').reversed;
+    final nodes =
+        reversedSumList.map((element) => ListNode(int.parse(element))).toList();
+
+    for (int i = 0; i < nodes.length - 1; i++) {
+      nodes[i].next = nodes[i + 1];
     }
 
-    return [];
+    finalListNode = nodes[0];
+
+    return finalListNode;
   }
 }
 
-// this class is for o(n log n) solution mean sorting the array first
-// then use two pointer to find the solution
-// the benfit of this solution is that it is more efficient than the first solution
-// but the benfit of the first solution is that it is more efficient than the second solution
-class SolutionSort {
-  List<int> twoSum(List<int> nums, int target) {
-    List<List<int>> indexedNums =
-        List.generate(nums.length, (index) => [nums[index], index]);
-    indexedNums.sort((a, b) => a[0].compareTo(b[0]));
+// the second solution is more efficient and scalable and maintainable and readable and use less memory and time
 
-    int left = 0;
-    int right = nums.length - 1;
+class Solution2 {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    ListNode dummy = ListNode(0);
+    ListNode current = dummy;
+    int carry = 0;
 
-    while (left < right) {
-      int currentSum = indexedNums[left][0] + indexedNums[right][0];
-      if (currentSum == target) {
-        return [indexedNums[left][1], indexedNums[right][1]];
-      } else if (currentSum < target) {
-        left++;
-      } else {
-        right--;
-      }
+    while (l1 != null || l2 != null || carry != 0) {
+      int x = l1?.val ?? 0;
+      int y = l2?.val ?? 0;
+      int sum = carry + x + y;
+      carry = sum ~/ 10;
+      current.next = ListNode(sum % 10);
+      current = current.next!;
+      l1 = l1?.next;
+      l2 = l2?.next;
     }
-    return [];
+
+    return dummy.next;
   }
 }
 
-// this class is for o(n^2) solution mean using two for loop to find the solution
-// this solution is not efficient because it is not efficient like another solution above
-// but it is a good solution to understand the problem and the solution
-class SolutionBruteForce {
-  List<int> twoSum(List<int> nums, int target) {
-    for (int i = 0; i < nums.length - 1; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return [i, j];
-        }
-      }
-    }
-    // If no solution is found
-    return [];
-  }
-}
-
-// this class is for o(n) solution mean using hashmap to store the value and the index of the number
-
-class SolutionHash {
-  List<int> twoSum(List<int> nums, int target) {
-    Map<int, int> numMap = {};
-
-    // First pass: Build the hash table
-    for (int i = 0; i < nums.length; i++) {
-      numMap[nums[i]] = i;
-    }
-
-    // Second pass: Check for complements
-    for (int i = 0; i < nums.length; i++) {
-      int complement = target - nums[i];
-      if (numMap.containsKey(complement) && numMap[complement] != i) {
-        return [i, numMap[complement]!];
-      }
-    }
-
-    // If no solution is found
-    return [];
-  }
-}
+// comment to psuh to github
